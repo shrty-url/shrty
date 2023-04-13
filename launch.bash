e2359f26854bd4eac47385a1d14f96734975dfc2
Moritz Utcke <62291876+Letsmoe@users.noreply.github.com>
2023-04-13 23:07:05 +0400

    Update launch.bash

--- launch.bash
+++ launch.bash
@@ -1,1 +1,3 @@
-touch ./.env-prod && echo "PRIVATE_KEY=$(cat /etc/letsencrypt/live/shrty.it/privkey.pem | base64 | tr -d '\n')" >> ./.env-prod && echo "CERTIFICATE=$(cat /etc/letsencrypt/live/shrty.it/fullchain.pem | base64 | tr -d '\n')" >> ./.env-prod && docker-compose -f docker-compose.prod.yml --env-file ./.env-prod up;
+docker-compose down;
+
+touch ./.env-prod && echo "PRIVATE_KEY=$(cat /etc/letsencrypt/live/shrty.it/privkey.pem | base64 | tr -d '\n')" >> ./.env-prod && echo "CERTIFICATE=$(cat /etc/letsencrypt/live/shrty.it/fullchain.pem | base64 | tr -d '\n')" >> ./.env-prod && docker-compose -f docker-compose.prod.yml --env-file ./.env-prod up -d;
